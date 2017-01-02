ed6f798c56549f7bcefcd3d03676e764c6ff1794
Brendan Frost <brendangfrost@gmail.com>
2017-01-02 17:09:54 -0600

    got text input working for jeebus sake

--- App/Components/UserMapCallout.js
+++ App/Components/UserMapCallout.js
@@ -31,6 +31,7 @@ export default class MapCallout extends React.Component {
     super(props)
     this.onPress = this.props.onPress.bind(null, this, this.props.location)
     this.state = { text: 'Useless Placeholder' }
+    this.handleChangeSubmit = this.handleChangeSubmit.bind(this);
   }
 
   shouldComponentUpdate (nextProps, nextState) {
@@ -44,8 +45,9 @@ export default class MapCallout extends React.Component {
      console.log(text, 'heres possible text in rounded button');
   }
 
-  handleChangeSubmit = (text) => {
-    this.setState({ text: text })
+  handleChangeSubmit (e) {
+    this.inputText = e.nativeEvent.text
+    console.log(this.inputText, 'why work for connor but no me')
   }
 
   render () {
@@ -62,7 +64,7 @@ export default class MapCallout extends React.Component {
         {/* <TouchableOpacity onPress={this.onPress}> */}
         <View>
           <TextInput
-          //  ref='text'
+           ref={input => { this.litterInput = input }}
           //  editable={editable}
           //  keyboardType='default'
           //  returnKeyType='next'
@@ -71,18 +73,17 @@ export default class MapCallout extends React.Component {
            maxLength = {200}
            multiline = {false}
            placeholder="Type a poem. Think a thought."
-           onChangeText={this.handleChangeSubmit}
-           value={this.state.text}
+           onChange={this.handleChangeSubmit}
+          //  value={this.state.text}
           //  onSubmitEditing
            numberOfLines = {1}
          />
          <RoundedButton
            text='Litter!'
-           onPress={
-             () => {
-             let text = this.state.text;
-             console.log(text, 'heres possible text in rounded button');
-             }}
+           onPress={() => {
+             console.log(this.inputText, 'heres input text on submit')
+             console.log(location.latitude, 'heres latitude')
+           }}
          />
           {/* <Text>{location.latitude}</Text> */}
         {/* </TouchableOpacity> */}
