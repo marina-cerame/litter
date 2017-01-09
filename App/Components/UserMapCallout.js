b4c68173f534ba48cf22f4bb6f97051386d8403f
cdakin1 <christophedakin1@gmail.com>
2017-01-09 15:16:31 -0600

    added photo picker

--- App/Components/UserMapCallout.js
+++ App/Components/UserMapCallout.js
@@ -1,21 +1,13 @@
 import React from 'react'
-import { Text, View, ScrollView, TouchableOpacity, TextInput } from 'react-native'
+import { Text, View, ScrollView, TouchableOpacity, TextInput, Image, Platform } from 'react-native'
 import MapView from 'react-native-maps'
 import Styles from './Styles/MapCalloutStyle'
 import ExamplesRegistry from '../Services/ExamplesRegistry'
 import RoundedButton from './RoundedButton'
+import { Images } from '../Themes';
+import ImagePicker from 'react-native-image-picker';
 var firebase = require('firebase')
-// var Litter = require('./models/litter');
 
-// Example
-// ExamplesRegistry.add('Map Callout', () =>
-//   <MapCallout
-//     location={{
-//       title: 'Callout Example'
-//     }}
-//     onPress={() => window.alert('That tickles!')}
-//   />
-// )
 
 type MapCalloutProps = {
   location: Object,
@@ -33,6 +25,7 @@ export default class MapCallout extends React.Component {
     this.onPress = this.props.onPress.bind(null, this, this.props.location)
     this.state = { text: 'Useless Placeholder' }
     this.handleChangeSubmit = this.handleChangeSubmit.bind(this);
+    this.handlePhoto = this.handlePhoto.bind(this);
   }
 
   shouldComponentUpdate (nextProps, nextState) {
@@ -50,6 +43,56 @@ export default class MapCallout extends React.Component {
     this.inputText = e.nativeEvent.text
     console.log(this.inputText, 'why work for connor but no me');
   }
+
+  handlePhoto() {
+    var options = {
+      title: 'Select Avatar',
+      customButtons: [
+        //TODO
+        {name: 'fb', title: 'Choose Photo from Facebook'},
+      ],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images'
+      }
+    };
+    /**
+     * The first arg is the options object for customization (it can also be null or omitted for default options),
+     * The second arg is the callback which sends object: response (more info below in README)
+     */
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        // TODO pick definition for source
+
+        // You can display the image using either data...
+        const source = {uri: 'data:image/jpeg;base64,' + response.data, isStatic: true};
+        // or a reference to the platform specific asset location
+        // if (Platform.OS === 'ios') {
+        //   const source = {uri: response.uri.replace('file://', ''), isStatic: true};
+        // } else {
+        //   const source = {uri: response.uri, isStatic: true};
+        // }
+        this.setState({
+          avatarSource: source
+        });
+      }
+    });
+  }
+
+
+
+
+
 
   render () {
     /* ***********************************************************
@@ -70,16 +113,23 @@ export default class MapCallout extends React.Component {
            onChange={this.handleChangeSubmit}
            numberOfLines = {1}
          />
-         <RoundedButton
-           text='Litter!'
-           onPress={() => {
-             firebase.database().ref('litter/').push({
-               text: this.inputText,
-               longitude: location.longitude,
-               latitude : location.latitude
-             });
-           }}
-         />
+         <View style={{flexDirection: 'row'}}>
+           <RoundedButton
+             style={{flex: 10}}
+             text='Litter!'
+             onPress={() => {
+               firebase.database().ref('litter/').push({
+                 text: this.inputText,
+                 longitude: location.longitude,
+                 latitude : location.latitude
+               });
+             }}
+           />
+           <RoundedButton style={{flex: 1}}
+           text='Photo'
+           onPress={this.handlePhoto} />
+         </View>
+
       </View>
       </MapView.Callout>
     )
