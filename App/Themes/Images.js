c10bcb4e97883095a3f90acbbc9ea9b4e0669640
Marina Cerame <marina.cerame@gmail.com>
2017-01-09 15:04:05 -0600

    fix logo, add photo button

--- App/Themes/Images.js
+++ App/Themes/Images.js
@@ -2,11 +2,12 @@
 
 // leave off @2x/@3x
 const images = {
-  logo: require('../Images/litter.png'),
+  logo: require('../Images/littersmall.png'),
   clearLogo: require('../Images/top_logo.png'),
   ignite: require('../Images/ignite_logo.png'),
   tileBg: require('../Images/tile_bg.png'),
-  background: require('../Images/BG.png')
+  background: require('../Images/BG.png'),
+  camera: require('../Images/camera.png')
 }
 
 export default images
